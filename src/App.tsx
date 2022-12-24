58806255102660fe46bba74c5c847c0cf94c1b73
Guilherme Sartori <gui.sartori96@gmail.com>
2022-12-24 18:02:55 -0300

    feat(app): create folder structure, globalStyles and project pattern

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { GlobalStyles } from './globalStyles'
+
+const App: React.FC = () =>
+	(
+		<>
+			<div className="App">
+			Olá, NOSCONFORMES!
+			</div>
+			<GlobalStyles />
+		</>
+	)
+
+
+export { App }
---
+++ src/globalStyles.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+  body {
+    background: #FFFFFF;
+    color: #0F141E;
+    -webkit-font-smoothing: antialiased;
+  }
+  button {
+    cursor: pointer;
+  }
+	#root {
+		min-height: 100vh;
+		max-width: 100vw;
+		display: flex;
+		flex-direction: column;
+		padding: 0;
+		overflow-y: hidden;
+	}
+`
+
+export { GlobalStyles }
