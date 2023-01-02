b68e3ef44fa596ad3c6bb911336911d312aa945a
Guilherme Sartori <gui.sartori96@gmail.com>
2023-01-02 16:28:25 -0300

    feat(knowledge base): create questionary card

---
+++ src/modules/knowledgeBase/Questionaries/QuestionariesList/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	max-width: 1300px;
+	margin: 0 auto;
+	padding: 60px 48px;
+
+	.questionaries-header-components {
+		display: flex;
+		justify-content: space-between;
+
+		.search-input {
+			width: 440px;
+			background: #FFFFFF;
+		}
+
+		.new-questionary-button {
+			width: 220px;
+		}
+	}
+
+	.all-questionaries {
+		padding-top: 16px;
+	}
+`
--- src/modules/knowledgeBase/Questionaries/index.tsx
+++ src/modules/knowledgeBase/Questionaries/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FirstNewQuestionarie } from './FirstNewQuestionarie'
+import { QuestionariesList } from './QuestionariesList'
 
 export const Questionaries: React.FC = () => {
+	const [questionaries, setQuestionaries] = useState(1)
+
+	const renderQuestionaries = () => {
+		if (questionaries === 0) {
+			return (
+				<FirstNewQuestionarie />
+			)
+		} else {
+			return <QuestionariesList />
+		}
+	}
+
 	return (
-		<p>olá, questionaries</p>
+		<>
+			{
+				renderQuestionaries()
+			}
+		</>
 	)
 }
