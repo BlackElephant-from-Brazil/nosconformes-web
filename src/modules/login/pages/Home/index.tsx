5b0107310e523fadfa2de1c7afa186b158a3e60c
Guilherme Sartori <gui.sartori96@gmail.com>
2023-01-05 13:47:21 -0300

    feat(api): creating connection via axios

--- src/modules/login/pages/Home/index.tsx
+++ src/modules/login/pages/Home/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../../../api'
 
 const Home: React.FC = () => {
+	const [user, setUser] = useState<any | undefined>()
+
+	useEffect(() => {
+		api
+			.get('/users')
+			.then((response) => setUser(response.data[0]))
+			.catch((err) => {
+				console.error('ops! ocorreu um erro' + err)
+			})
+	}, [])
+
+
+
 	return (
 		<div>
-			Olá, NOSCONFORMES!
+			{
+				user?.name
+			}
 		</div>
 	)
 }
